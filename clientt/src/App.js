e81e30b51d1e4c297bec94fcb823335c496f1556
Vinh <55190465+kieuvinhh@users.noreply.github.com>
2021-06-30 23:03:45 +0700

    'fs'

--- clientt/src/App.js
+++ clientt/src/App.js
@@ -3,10 +3,18 @@ import './style.css'
 import React , { useState, useRef }from 'react'
 
 function App () {
-
+  const lyrics = [
+    "Look up in the mirror, oh my God, it's me",
+    "So much Prada on me, I'm a Pradagy",
+    "I'ma do my thing, no apologies",
+    "Coconut and rose in my skin regime",
+    "Mirror, mirror on the wall, tell me what you see",
+    "It's that, oh my God, it's lookin' heavenly (ooh)",
+  ]
   const [image, setImage] = useState();
   const inputFile = useRef();
-
+  const [result, setResult] = useState("Result");
+  const [idx, setIdx] = useState(0); 
   const handleFileUpload = (e) => {
     const filename = e.target.files[0];
 
@@ -25,12 +33,22 @@ function App () {
   return (
     <div className="App">
       <input ref={inputFile} onChange={handleFileUpload} type="file" hidden />
-      <button onClick={BtnOnClick} className="App__upload">
-      upload
+      <button onClick={BtnOnClick} className="App__upload" >
+      Upload
       </button>
       <img alt="" src={image} className="App__image" />
 
-      
+      <button
+        onClick={() => {
+          setIdx(() => (idx + 1) % lyrics.length);
+          setResult(lyrics[idx]);
+        }}
+      >
+        Test buton
+      </button>
+      <div className="App__result">
+        <p className="App__result__text">{result}</p>
+      </div>
     </div>
   );
 }
--- clientt/src/components/resultcpn.js
+++ clientt/src/components/resultcpn.js
@@ -1,10 +1,8 @@
 import React from 'react'
 
 const Resultcpn = (props) => {
-    return ( <
-        h1 > result Component < /h1>
-    )
-}
+        return ( <h1> result Component </h1>)
+        }
 
 
-export default Resultcpn;
+        export default Resultcpn;

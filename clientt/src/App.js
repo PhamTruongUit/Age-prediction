403a68b026151d0235429b83e8277e0cad6187b9
Vinh <55190465+kieuvinhh@users.noreply.github.com>
2021-06-30 22:08:46 +0700

    'completeImage'

--- clientt/src/App.js
+++ clientt/src/App.js
@@ -1,18 +1,38 @@
 import './App.css';
 import './style.css'
-import React from 'react'
-import Imagecpn from './components/imagecpn.js'
-import Resultcpn from './components/resultcpn.js'
-import Buttoncpn from './components/buttoncpn';
-const App = () => {
-  return (
-    <div >
+import React , { useState, useRef }from 'react'
 
-    <Buttoncpn />
-    <Imagecpn />
-    <Resultcpn />
+function App () {
+
+  const [image, setImage] = useState();
+  const inputFile = useRef();
+
+  const handleFileUpload = (e) => {
+    const filename = e.target.files[0];
+
+    const reader = new FileReader();
+
+    reader.onloadend = () => {
+      setImage(reader.result);
+    };
+
+    reader.readAsDataURL(filename);
+  };
+
+  const BtnOnClick = () => {
+    inputFile.current.click();
+  };
+  return (
+    <div className="App">
+      <input ref={inputFile} onChange={handleFileUpload} type="file" hidden />
+      <button onClick={BtnOnClick} className="App__upload">
+      upload
+      </button>
+      <img alt="" src={image} className="App__image" />
+
+      
     </div>
-    )
+  );
 }
 
 
--- clientt/src/components/resultcpn.js
+++ clientt/src/components/resultcpn.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
 const Resultcpn = (props) => {
-    return ( 
-        <h1> result Component </h1>
+    return ( <
+        h1 > result Component < /h1>
     )
 }
 

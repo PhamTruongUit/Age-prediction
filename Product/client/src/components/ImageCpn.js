2fc4c63dd443e12061142b0c053d621a104c574c
Vinh <55190465+kieuvinhh@users.noreply.github.com>
2021-07-13 17:27:50 +0700

    'fixcanvas'

--- Product/client/src/components/ImageCpn.js
+++ Product/client/src/components/ImageCpn.js
@@ -5,6 +5,7 @@ import axios from "axios";
 
 export default function ImageCpn() {
   const [result, setResult] = useState("kết quả");
+  const [Url, setUrl] = useState("");
   const [resultStyle, setResultStyle] = useState("alert alert-primary")
   const [image, setImage] = useState(null);
 
@@ -14,37 +15,39 @@ export default function ImageCpn() {
   function getBase64Image(img) {
     // https://stackoverflow.com/questions/22172604/convert-image-url-to-base64
     var canvas = document.createElement("canvas");
-    canvas.width = img.width;
-    canvas.height = img.height;
+    canvas.width = img.naturalWidth;
+    canvas.height = img.naturalHeight;
+    console.log("Kich thuoc canvas", canvas.width, canvas.height);
     var ctx = canvas.getContext("2d");
-    ctx.drawImage(img, 0, 0);
+    ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
     var dataURL = canvas.toDataURL("image/png");
     return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
   }
   
   
   function onImageLoaded(e){
-    var base64 = getBase64Image(e.target);
-    const url = 'http://98a1a82744c0.ngrok.io/image' // post to this url
+    const img = e.target;
+    var base64 = getBase64Image(img);
     var bodyFormData = new FormData();
     bodyFormData.append('img', base64);
   
 axios({
       method: "post",
-      url: url,
+      url: Url,
       data: bodyFormData
     })
       .then(function (response) {
-        const res_age = response['data'].age; 
+        const res_age = response['data'].age;
+        const res_cl = response['data'].cl_age; 
         // console.log(res_age);
-        const resultss = 'Độ tuổi của người trong ảnh là: ' + res_age;
+        const resultss = 'Độ tuổi: ' + res_age + ' Nhóm: ' + res_cl;
         setResult(resultss);
         setResultStyle("alert alert-primary") // Chuyen khung result sang mau xanh
       })
       .catch(function (error){
         console.log(error);
         console.log("Cannot detect face")
-        setResult("Sorry! We can't find any face in this image")
+        setResult("Sorry! Can not find any face in this image")
         setResultStyle("alert alert-danger") // Chuyen khung result sang mau do
       })
   }
@@ -52,6 +55,8 @@ axios({
  
   return (
     <div className='App_image'>
+    <input id='input-url' type="text" name="name" autoComplete='off'/>
+    <button onClick={ () => {setUrl(document.getElementById('input-url').value)}}>update Url</button>
       <MagicDropZone
         accept="image/jpeg, image/png, .jpg, .jpeg, .png"
         onDrop={onDrop} 
@@ -67,7 +72,6 @@ axios({
       </MagicDropZone>
 
       <div className={resultStyle} > {result}</div>
-     
     </div>
   );
 }

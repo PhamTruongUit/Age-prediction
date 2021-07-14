c96f9533ddbb7bd892102d4222f8401e90f7a62a
Vinh <55190465+kieuvinhh@users.noreply.github.com>
2021-07-14 14:06:43 +0700

    'sv

--- Product/sever/app.py
+++ Product/sever/app.py
@@ -1,14 +1,29 @@
+import numpy as np
 from flask import Flask
 from flask_cors import CORS, cross_origin
+from flask import request, jsonify
 from tensorflow.keras.models import load_model
+import tensorflow as tf
+import cv2
 from PIL import Image
-import numpy as np
+import base64
+from flask_ngrok import run_with_ngrok
 from deepface import DeepFace
-
+import matplotlib.pyplot as plt
 app = Flask(__name__)
-
+run_with_ngrok(app) # ngrok colab
 
 model = load_model('MobileNetV3Large.h5')
+print(model.summary())
+groups_names= ['Children', 'Teenage', 'Youth', 'Middle', 'Old']
+
+
+def predict_class(age):
+  if age <= 10: return groups_names[0] #Children 0 - 10
+  if age <= 18: return groups_names[1] #Teenage 10 - 18
+  if age <= 34: return groups_names[2] #Youth   19 - 34
+  if age <= 60: return groups_names[3] #Middle  35 - 60
+  return groups_names[4]               #Old     61+
 
 
 def ConvBase64toImage(img_base64):
@@ -30,7 +45,7 @@ def preprocess_image(data):
 
 def detect_face(img_path):
     backends = ['opencv', 'ssd', 'dlib', 'mtcnn', 'retinaface']
-    detected_face = DeepFace.detectFace( img_path , detector_backend=backends[2])
+    detected_face = DeepFace.detectFace( img_path , detector_backend='dlib')
     return detected_face
 
 
@@ -42,11 +57,23 @@ def predict_age(image):
 CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-@app.route('/', methods=['POST'])
+@app.route('/image', methods=['POST'])
 @cross_origin(origin='*')
 def process():
     img_arg_base64 = request.form.get('img')
-    img = ConvBase64toImage(img_arg_base64)
-    img = detect_face(img)
-    age = predict_age(img)
-    return str(age)
+    img1 = ConvBase64toImage(img_arg_base64)
+    img2 = detect_face(img1)
+    age = predict_age(img2)
+    cl_age = predict_class(age)
+    result = {
+        'age': age,
+        'cl_age': cl_age
+        }
+    return result
+    
+@app.route('/', methods=['GET'])
+@cross_origin(origin='*')
+def index():
+  return "Hello day la sever from colab"
+if __name__ == '__main__':
+  app.run()
